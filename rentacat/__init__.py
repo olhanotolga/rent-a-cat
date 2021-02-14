70b4d96676308354106704657628df924405a8fc
Olha Halat <olha.halat@gmail.com>
2021-02-14 02:01:11 +0100

    add routes, forms, and registration+login pages

--- rentacat/__init__.py
+++ rentacat/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '3fb9e6ad18e3208bd36e1cdd0c3e21f7'
+
 from rentacat import routes
--- rentacat/routes.py
+++ rentacat/routes.py
@@ -1,7 +1,46 @@
+from flask import render_template, url_for, flash, redirect
 from rentacat import app
-from flask import render_template
+from rentacat.forms import RegistrationForm, LoginForm
 
 @app.route("/")
+@app.route("/home")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", title="Home", h1="Rent a Cat")
+
+@app.route("/about")
+def about():
+	return render_template("about.html", title="About")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@rac.com' and form.password.data == 'qwerty':
+			flash('You\'re in!', 'success')
+			#! REDIRECT to profile or views later
+			return redirect(url_for('index'))
+		else:
+			flash('Nope. Please check if your email and/or password are correct.', 'danger')
+	return render_template("login.html", title="Log in", form=form)
+
+@app.route("/signup", methods=["GET", "POST"])
+def registration():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Welcome aboard, {form.username.data}!', 'success')
+		#! REDIRECT to profile or views later
+		return redirect(url_for('index'))
+	return render_template("registration.html", title="Sign up", form=form)
+
+@app.route("/profile")
+def profile():
+	return render_template("profile.html", title="Your profile")
+
+@app.route("/view")
+def view():
+	return render_template("view.html", title="View")
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template("notfound.html", title="Not found"), 404
